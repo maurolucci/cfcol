adc3bddd565dc57c21c06ffb568ea012b307e6d9
Mauro Lucci <maurolucci.14@gmail.com>
2026-03-10 17:11:19 -0300

    Select the number of vertices based on the edge probability

--- instances/script_gen_random_dpcp1.sh
+++ instances/script_gen_random_dpcp1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a N=("100" "150")
+declare -a N=("130" "150" "170")
 declare -a P=("0.25" "0.5" "0.75")
 declare -a NA=("0.1" "0.2")
 declare -a NB=("0.1" "0.2")
@@ -12,23 +12,25 @@ OUT="dpcp/random"
 mkdir -p dpcp
 mkdir -p dpcp/random
 
-for n in "${N[@]}"
+for i in "${!N[@]}"
 do
-    for p in "${P[@]}"
-    do
-        for na in "${NA[@]}"
-        do
-            for nb in "${NB[@]}"
-            do
-                naa=$(echo "scale=0; ($na * $n + 0.5)/1" | bc)
-                nbb=$(echo "scale=0; ($nb * $n + 0.5)/1" | bc)
-                echo "n: $n, p: $p, nA: $naa, nB: $nbb, i: $i"
-                python3 $SRC $n $p $naa $nbb $num $OUT
-                for i in $(seq 0 $((num - 1)))
-                do
-                    echo "r_n${n}_p${p}_nA${naa}_nB${nbb}_i${i}.dpcp" >> "$OUT/instances.txt"
-                done
-            done
+    n="${N[$i]}"
+    p="${P[$i]}"
+    for na in "${NA[@]}"
+	do
+	    if [[ $p == "0.25" && $na == "0.1" ]]; then
+			continue
+		fi
+		for nb in "${NB[@]}"
+	    do
+		    naa=$(echo "scale=0; ($na * $n + 0.5)/1" | bc)
+		    nbb=$(echo "scale=0; ($nb * $n + 0.5)/1" | bc)
+		    echo "n: $n, p: $p, nA: $naa, nB: $nbb, i: $i"
+		    python3 $SRC $n $p $naa $nbb $num $OUT
+		    for i in $(seq 0 $((num - 1)))
+		    do
+		        echo "r_n${n}_p${p}_nA${naa}_nB${nbb}_i${i}.dpcp" >> "$OUT/instances.txt"
+		    done
         done
     done
 done
